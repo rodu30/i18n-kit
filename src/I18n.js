0b80f7e81ad4eaabf3c4260cb648acb806c13157
Roman Duhr <roman.duhr@meplato.com>
2017-11-22 18:43:30 +0100

    more functionality added to I18n class

--- src/I18n.js
+++ src/I18n.js
@@ -1,5 +1,6 @@
 import formatNumber from './number';
 import formatDateTime from './dateTime';
+import formatMessage, { addNewMessage } from './message';
 
 /**
  *
@@ -9,51 +10,117 @@ import formatDateTime from './dateTime';
  */
 export default class I18n {
   /**
+   * Constructor for instance of this class,
+   * passing the object for messages and an initial default locale is required,
+   * passing a message locale is optional
+   * NOTE: If provided, messages passed to formatMessage will automatically be added to this locale,
+   * which should be the standard use case. If not set, messages will be displayed but not saved.
+   * A possible use case could be when you wish to define the messages manually and only use the
+   * default message as fallback. In this case you can disable all warnings in the message options.
+   * TODO: does that make sense??
    *
-   * @param {*} messages
-   * @param {*} initialLocale
-   * @param {*} defaultLocale if provided, default messages passed with formatMessage will automatically be added to this locale
+   * @param {object} messages
+   * @param {string} defaultLocale
+   * @param {string} messageLocale
+   * @param {object} options use a keys 'messageOptions', 'numberOptions', 'dateTimeOptions' to specify
    */
-  constructor(messages, initialLocale, defaultLocale) {
+  constructor(
+    messages,
+    defaultLocale,
+    messageLocale = undefined,
+    { messageOptions = {}, numberOptions = {}, dateTimeOptions = {} }
+  ) {
     this.messages = messages;
-    this.currentLocale = initialLocale;
-    this.messageOptions = { defaultLocale };
-    this.numberOptions = {};
-    this.dateTimeOptions = {};
+    this.locale = defaultLocale;
+    this.defaultLocale = defaultLocale;
+    this.messageOptions = { ...messageOptions, messageLocale };
+    this.numberOptions = numberOptions;
+    this.dateTimeOptions = dateTimeOptions;
   }
 
-  changeLocale = locale => {
-    this.currentLocale = locale;
+  /**
+   * Update current locale e.g. when user settings change
+   * @memberof I18n
+   * @param {string} locale
+   */
+  setLocale = locale => {
+    this.locale = locale;
+    // console.log(this);
   };
 
-  // TODO add more
-  setMessageOptions = ({ defaultLocale, noWarnings }) => {
-    if (this.messageOptions.defaultLocale) {
-      this.messageOptions.defaultLocale = defaultLocale;
-    }
+  setLocaleToDefault = () => {
+    this.locale = this.defaultLocale;
+    // console.log(this);
   };
 
+  /**
+   * Set or update options for formatting messages in your instance of this class
+   * @memberof I18n
+   * @param {object} options
+   */
+  // TODO: add more defined definition
+  setMessageOptions = options => {
+    this.messageOptions = { ...this.messageOptions, ...options };
+    // console.log(this);
+  };
+
+  /**
+   * Set or update options for formatting numbers in your instance of this class
+   * @memberof I18n
+   * @param {object} options
+   */
+  // TODO: add more defined definition
   setNumberOptions = options => {
     this.numberOptions = { ...this.numberOptions, ...options };
+    // console.log(this);
   };
 
+  /**
+   * Set or update options for formatting date objects in your instance of this class
+   * @memberof I18n
+   * @param {object} options
+   */
+  // TODO: add more defined definition
   setDateTimeOptions = options => {
     this.dateTimeOptions = { ...this.dateTimeOptions, ...options };
+    // console.log(this);
   };
 
   /**
-   *
-   * NOTE: options if provided override class options
+   * Format a number according to current locale
+   * NOTE: if provided, options override class options
    * @memberof I18n
+   * @param {number} number
+   * @param {object} options
    */
   formatNumber = (number, options) =>
-    formatNumber(this.currentLocale, number, { ...this.numberOptions, ...options });
+    formatNumber(this.locale, number, options || this.numberOptions);
 
   /**
-   *
-   * NOTE: options if provided override class options
+   * Format a Date object according to current locale
+   * NOTE: if provided, options override class options
    * @memberof I18n
+   * @param {object} date
+   * @param {object} options
    */
   formatDateTime = (date, options) =>
-    formatDateTime(this.currentLocale, date, { ...this.numberOptions, ...options });
+    formatDateTime(this.locale, date, options || this.dateTimeOptions);
+
+  /**
+   * Format a ICU string with optional variables and
+   * return translation if current locale is not the default locale
+   * NOTE: if provided, options override class options
+   * @memberof I18n
+   * @param {string} message
+   * @param {object} options
+   */
+  formatMessage = (message, options) => {
+    // If messageLocale is set, add message to messages of default locale
+    const messageLocale = options.messageLocale || this.messageOptions.messageLocale;
+    if (messageLocale) {
+      this.messages = addNewMessage(this.messages, messageLocale, message);
+    }
+    console.log(this);
+    return formatMessage(this.locale, this.messages, message, options || this.messageOptions);
+  };
 }
--- src/message.js
+++ src/message.js
@@ -1,20 +1,46 @@
 // --- Helper for formatting and adding messages ---
 
 /**
+ * Generates a string from the provided message containing placeholders (according to ICU standard) and an object with variables
+ * @param {string} message
+ * @param {object} vars
+ * @returns {string}
+ */
+const generateStringWithVars = (message, vars = {}) => {
+  let newMessage = message;
+  Object.entries(vars).forEach(([key, value]) => {
+    const pattern = new RegExp(`{${key}}`, 'g');
+    newMessage = newMessage.replace(pattern, value);
+  });
+  return newMessage;
+};
+
+/**
+ * Generates a key from a given message
+ * @param {string} message
+ */
+export const generateKeyFromString = message =>
+  message
+    .toLowerCase()
+    .replace(/{|}|\.|,|\?|\!/g, '')
+    .replace(/ /g, '_');
+// TODO: add more RegEx to remove special characters
+
+/**
  * Adds new message and new locale (if necessary) to a given message object
  * @param {object} messages
  * @param {string} locale
  * @param {string} message
- * @param {string} description ???
+ * @returns {object} new messages
  */
-const addNewMessage = (messages, locale, message, description) => {
+export const addNewMessage = (messages, locale, message) => {
   // New message key
   const newKey = generateKeyFromString(message);
 
   // Clone messages
-  let newMessages = { ...messages };
+  const newMessages = { ...messages };
 
-  // If locale doesn't exist, add it (?)
+  // If locale doesn't exist, add it and then message to it
   if (!messages[locale]) {
     newMessages[locale] = locale;
     console.info(`A new locale "${locale}" was added.`);
@@ -30,72 +56,49 @@ const addNewMessage = (messages, locale, message, description) => {
     newMessages[locale][newKey] = message;
     console.info(`A new message "${message}" was added to locale "${locale}".`);
   }
-};
-
-/**
- * Generates a key from a given message
- * @param {string} message
- */
-export const generateKeyFromString = message => {
-  return message
-    .toLowerCase()
-    .replace(/{|}|\.|,|\?|\!/g, '')
-    .replace(/ /g, '_');
-  // TODO: add more RegEx to remove special characters
-};
-
-/**
- * Generates a string from the provided message containing placeholders (according to ICU standard) and an object with variables
- * @param {string} message
- * @param {object} vars
- * @returns {string}
- */
-const generateStringWithVars = (message, vars = {}) => {
-  let newMessage = message;
-  Object.entries(vars).forEach(([key, value]) => {
-    const pattern = new RegExp(`{${key}}`, 'g');
-    newMessage = newMessage.replace(pattern, value);
-  });
-  return newMessage;
+  return newMessages;
 };
 
 // --- Format text ---
 
 /**
- * Formats a given default string or it`s translation (according to locale) and optional placeholder
+ * Formats a given string or
+ * string`s translation (according to locale) with optional placeholder variables (ICU syntax)
  * @param {string} locale
  * @param {object} messages
- * @param {string} defaultMessage
+ * @param {string} message
  * @param {object} options:
- *                 {object} vars variables for placeholder in defaultMessage
- *                 {string} defaultLocale if provided, the defaultMessage will automatically be added to this locale
+ *                 {object} vars variables for placeholder in message
+ *                 {string} messageLocale if provided, the message will automatically be added to this locale
  *                 {string} description
  *                 {boolean} noWarnings
  * @returns {string}
  */
-const formatMessage = (locale, messages, defaultMessage, options = {}) => {
+const formatMessage = (locale, messages, message, options = {}) => {
   // Deconstruct options
-  const { vars, defaultLocale, description, noWarnings } = options;
+  const { vars, messageLocale, description, disableWarnings } = options;
 
   // Get complete string with vars
-  const defaultWithVars = generateStringWithVars(defaultMessage, vars);
+  const defaultWithVars = generateStringWithVars(message, vars);
 
-  // If defaultLocale is set, add defaultMessage to messages of default locale
-  if (defaultLocale) {
-    addNewMessage(messages, defaultLocale, defaultMessage, description);
-  }
+  // // If messageLocale is set, add message to messages of default locale
+  // if (messageLocale) {
+  //   addNewMessage(messages, messageLocale, message);
+  // }
 
   // If requested locale doesn't exist yet return default (+ warning)
-  if (!messages.hasOwnProperty(locale)) {
-    console.warn(
-      `Warning: Displaying default message\nThere are no translations for "${
-        locale
-      }". Please add it to your list of locales.`
-    );
+  if (!messages[locale]) {
+    if (!disableWarnings) {
+      console.warn(
+        `Warning: Displaying default message\nThere are no translations for "${
+          locale
+        }". Please add it to your list of locales.`
+      );
+    }
     return defaultWithVars;
   }
 
-  const messageKey = generateKeyFromString(defaultMessage);
+  const messageKey = generateKeyFromString(message);
   const existingMessage = Object.entries(messages[locale]).find(
     ([key, value]) => key === messageKey
   );
@@ -103,23 +106,22 @@ const formatMessage = (locale, messages, defaultMessage, options = {}) => {
   // If message key doesn't exist in requested locale...
   if (!existingMessage) {
     // ...and locale is default locale, returns default (no warning b/c message was already added)
-    if (defaultLocale && defaultLocale === locale) return defaultWithVars;
+    if (messageLocale && messageLocale === locale) return defaultWithVars;
 
     // ...return default (+ warning)
-    console.warn(
-      `Warning: Displaying default message\n"${
-        defaultWithVars
-      }" has not been translated to requested locale "${locale}". Please add translation.`
-    );
+    if (!disableWarnings) {
+      console.warn(
+        `Warning: Displaying default message\n"${
+          defaultWithVars
+        }" has not been translated to requested locale "${locale}". Please add translation.`
+      );
+    }
     return defaultWithVars;
-  }
-
-  // If message key doesn't exist in requested locale but defaultLocale is set, add to default messages and return default (no warning)
-  if (!existingMessage && defaultLocale) {
   }
 
   // If message key exists in requested locale return message according to provided locale
   if (existingMessage) return generateStringWithVars(existingMessage[1], vars);
+
   // just in case
   return null;
 };
